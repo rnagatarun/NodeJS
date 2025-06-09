604cdbb3c0bbe7807733a2d25bcd91216068f65c
NagaTarun <rnagatarun@gmail.com>
2025-06-09 19:38:37 +0530

    explore global object on NodeJS

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "Hellooo Naga Tarun !!";
+var a=10;
+var b=20;
+console.log(name);
+console.log(a+b);
+console.log("Is globalThis === global: ",globalThis === global )
+
+console.log(global);
