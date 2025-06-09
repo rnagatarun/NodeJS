0b0f912153994bec92b5a3ffc9d9b025fd862f90
NagaTarun <rnagatarun@gmail.com>
2025-06-09 20:19:44 +0530

    import calculateSum from by using require keyword

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+require("./xyz.js")
+
 var name = "Hellooo Naga Tarun !!";
 var a=10;
 var b=20;
 console.log(name);
-console.log(a+b);
+ 
+//Import calculateSum method from module.exports from Sum.js
+const { calculateSum } = require("./sum.js")
+calculateSum(a,b);
+// console.log(a+b);
 console.log("Is globalThis === global: ",globalThis === global )
 
 console.log(global);
