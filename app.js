6fb6aef572eaf4cf3c65648bae78214d325dddea
NagaTarun <rnagatarun@gmail.com>
2025-06-10 23:14:57 +0530

    import sum and multiply from calculate

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var name = "Hellooo Naga Tarun !!";
 var a=10;
 var b=20;
 console.log(name);
+
+const{calculateMultiply, calculateSum} = require("./calculate")
  
-//Import calculateSum method from module.exports from Sum.js
-// const { calculateSum } = require("./sum.js")
-import { calculateSum } from "./sum.js";
-calculateSum(a,b);
+// import { calculateSum } from "./sum.js"; // es6 module import
+// calculateSum(a,b);
 // console.log(a+b);
-console.log("Is globalThis === global: ",globalThis === global )
+console.log("Is globalThis === global: ",globalThis === global );
+
+// const calculateMultiply = require("./calculate/multiply.js");
+calculateSum(a,b);
+calculateMultiply(a,b);
 
 console.log(global);
